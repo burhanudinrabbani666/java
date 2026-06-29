b0e27f827e6edf9f5e60cb86031dc01bbf4eee57
burhanudinrabbani666 <burhanudinrabbani666@gmail.com>
2026-06-29 07:47:09 +0700

    main in java 25

---
+++ mastering_modern_java_programming/HelloWorld.java
@@ -0,0 +1,14 @@
+// This is Java 25
+/**
+ * Dont use this in production.
+ * void main() {
+ * IO.println("Hello world");
+ * }
+ */
+
+// This is traditional Java
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+    }
+}
