c7186d82c7611a8382cf0dda3dab67b423e55da3
burhanudinrabbani666 <burhanudinrabbani666@gmail.com>
2026-06-29 10:38:45 +0700

    Create non static methods

---
+++ mastering_modern_java_programming/Section4/src/ArithmaticOperations.java
@@ -0,0 +1,7 @@
+public class ArithmaticOperations {
+
+    public Integer sum(Integer num1, Integer num2) {
+        return num1 + num2;
+    }
+
+}
--- mastering_modern_java_programming/Section4/src/Employee.java
+++ mastering_modern_java_programming/Section4/src/Employee.java
@@ -1,8 +1,18 @@
 public class Employee {
 
+    // VARIABLE
     String firstName;
     String lastName;
     byte age;
     char gender;
+
+    // METHODS
+    public byte getAge() {
+        return this.age;
+    }
+
+    public char getGender() {
+        return this.gender;
+    }
 
 }
