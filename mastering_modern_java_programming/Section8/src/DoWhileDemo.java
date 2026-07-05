0397e346533c917a13a09ffb4caa2cad029e451e
burhanudinrabbani666 <burhanudinrabbani666@gmail.com>
2026-07-05 22:36:15 +0700

    forloopdemo

--- mastering_modern_java_programming/Section8/src/DoWhileDemo.java
+++ mastering_modern_java_programming/Section8/src/DoWhileDemo.java
@@ -4,9 +4,7 @@ public class DoWhileDemo {
         int bensin = 100;
 
         do {
-            System.out.println(String.format(
-                    "Gassssss, bensin masih %d", bensin));
-
+            System.out.printf("Gassssss, bensin masih %d", bensin);
             bensin -= 10;
         } while (bensin > 0);
 
---
+++ mastering_modern_java_programming/Section8/src/ForLoopDemo.java
@@ -0,0 +1,14 @@
+public class ForLoopDemo {
+    public static void main(String[] args) {
+
+        int result = 0;
+        // basic for loop
+        for (int i = 1; i <= 10; i++) {
+            result = i * 5;
+            System.out.printf("5 * %d = %d\n", i, result);
+        }
+
+        // nested for loop
+
+    }
+}
