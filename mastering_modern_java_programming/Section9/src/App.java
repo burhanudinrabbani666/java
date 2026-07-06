c039558b78ae09456fdbeee288889163c8e55760
burhanudinrabbani666 <burhanudinrabbani666@gmail.com>
2026-07-06 09:40:05 +0700

    package in practice

---
+++ mastering_modern_java_programming/Section9/src/App.java
@@ -0,0 +1,10 @@
+import com.eazybytes.model.Employee;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+
+        Employee newEmployee = new Employee("burhanudin rabbani");
+        System.out.println(newEmployee);
+    }
+}
---
+++ mastering_modern_java_programming/Section9/src/com/eazybytes/app/MainApp.java
@@ -0,0 +1,12 @@
+package com.eazybytes.app;
+
+import com.eazybytes.model.Vehicle;
+import com.eazybytes.model.Employee;;
+
+public class MainApp {
+    public static void main(String[] args) {
+        Vehicle nissanGtr = new Vehicle();
+        Employee newEmployee = new Employee("burhanudin rabbani");
+
+    }
+}
