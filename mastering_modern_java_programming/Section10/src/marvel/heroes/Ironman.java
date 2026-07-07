0e8e026aa52614a98c9667b7aacbc71f3de9994c
burhanudinrabbani666 <burhanudinrabbani666@gmail.com>
2026-07-07 20:00:31 +0700

    overriding in practice

--- mastering_modern_java_programming/Section10/src/marvel/heroes/Ironman.java
+++ mastering_modern_java_programming/Section10/src/marvel/heroes/Ironman.java
@@ -8,4 +8,9 @@ public class Ironman extends Person {
         System.out.println("Ironman using his power");
     }
 
+    @Override // annotation
+    public void walk() {
+        System.out.println("Ironman walking so fast with nitro shoes");
+    }
+
 }
