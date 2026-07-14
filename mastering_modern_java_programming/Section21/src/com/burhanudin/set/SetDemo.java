3471a4c3f5555540915be4338652f8e2091ca33a
burhanudinrabbani666 <burhanudinrabbani666@gmail.com>
2026-07-14 18:37:04 +0700

    hashset in practice

---
+++ mastering_modern_java_programming/Section21/src/com/burhanudin/set/SetDemo.java
@@ -0,0 +1,40 @@
+package com.burhanudin.set;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Random;
+import java.util.Set;
+
+public class SetDemo {
+    public static void main(String[] args) {
+
+        List<Integer> myList = new ArrayList<>();
+        Random random = new Random();
+
+        for (int index = 0; index < 10; index++) {
+            myList.add(random.nextInt(10));
+        }
+
+        Collections.sort(myList);
+        System.out.println("Original Array: " + myList);
+
+        Set<Integer> newSet = new HashSet<>(myList);
+        System.out.println("HashSet: " + newSet);
+
+        System.out.println("---------------------------------------------");
+
+        Set<String> visitedCountry = new HashSet<>();
+
+        visitedCountry.add("Indonesia");
+        visitedCountry.add("Malaysia");
+        visitedCountry.add("Thailand");
+        visitedCountry.add("Singapore");
+        visitedCountry.add("Indonesia");
+
+        System.out.println(visitedCountry.size());
+        System.out.println(visitedCountry);
+
+    }
+}
