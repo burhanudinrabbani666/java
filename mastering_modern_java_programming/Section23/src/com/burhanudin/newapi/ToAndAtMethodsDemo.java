10dd652a440b6d2782aacc5fea1a91cc5ae2c0da
burhanudinrabbani666 <burhanudinrabbani666@gmail.com>
2026-07-15 12:42:49 +0700

    to and at in practice

---
+++ mastering_modern_java_programming/Section23/src/com/burhanudin/newapi/ToAndAtMethodsDemo.java
@@ -0,0 +1,30 @@
+package com.burhanudin.newapi;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.Month;
+
+public class ToAndAtMethodsDemo {
+    public static void main(String[] args) {
+        // IMMUTABLE OBJECT
+        LocalDateTime dateTime = LocalDateTime.of(2015, Month.DECEMBER, 18, 22, 30);
+        System.out.println(dateTime);
+
+        // TO
+        LocalDate localDate = dateTime.toLocalDate();
+        LocalTime localTime = dateTime.toLocalTime();
+
+        System.out.println(localDate);
+        System.out.println(localTime);
+
+        // AT
+        LocalDate localDateAt = LocalDate.of(2015, 3, 18);
+        LocalDateTime startDayOf = localDateAt.atStartOfDay();
+        System.out.println(startDayOf);
+
+        LocalDateTime localDateTime = localDateAt.atTime(18, 20);
+        System.out.println(localDateTime);
+
+    }
+}
