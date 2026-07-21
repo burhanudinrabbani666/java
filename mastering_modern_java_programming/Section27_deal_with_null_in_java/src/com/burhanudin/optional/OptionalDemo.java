ceee64e32e59db853713eb02b04518f4c347996d
burhanudinrabbani666 <burhanudinrabbani666@gmail.com>
2026-07-21 19:49:19 +0700

    Optional in practice

--- mastering_modern_java_programming/Section27_deal_with_null_in_java/src/com/burhanudin/optional/OptionalDemo.java
+++ mastering_modern_java_programming/Section27_deal_with_null_in_java/src/com/burhanudin/optional/OptionalDemo.java
@@ -1,14 +1,23 @@
 package com.burhanudin.optional;
 
+import java.util.Optional;
 import java.util.Random;
 
 public class OptionalDemo {
     public static void main(String[] args) {
         String message = sayHello();
 
-        // primitive
+        // PRIMITIVE
         if (message != null) {
             System.out.println(message.toUpperCase());
+        }
+
+        Optional<String> message2 = sayHi();
+
+        // validate with isEmpty() before get()
+        if (!message2.isEmpty()) {
+            System.out.println(message2
+                    .get().toUpperCase());
         }
 
     }
@@ -23,4 +32,17 @@ public class OptionalDemo {
 
         return null;
     }
+
+    public static Optional<String> sayHi() {
+        int randomNum = new Random().nextInt(10);
+        String msg = null;
+
+        if (randomNum % 2 == 0) {
+            msg = "Hi World";
+        }
+
+        System.out.println(randomNum);
+        return Optional.ofNullable(msg);
+    }
+
 }
